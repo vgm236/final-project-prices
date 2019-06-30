db2649a12e14b1811083f037afa2f963cf30995c
vgm236 <50808841+vgm236@users.noreply.github.com>
2019-06-30 17:15:13 -0400

    almost_finished_now_select_correct_Chart

--- app/inflation_new.py
+++ app/inflation_new.py
@@ -18,13 +18,6 @@ import sys
 
 import itertools
 
-
-
-########## ---> TODO: transform series (original, mom or yoy)
-########## ---> TODO: return error if series do not exist
-########## ---> TODO: Compare series
-
-
 ## Loading the data into the environment
 
 load_dotenv() #loads contents of the .env file into the script's environment
@@ -90,16 +83,6 @@ except:
     print("-------------------------------------")
     exit()
 
-#if str(input_code) not in code:
-#    print("-------------------------------------")
-#    print("\n")
-#    print("Non-existent code. Please try again.")
-#    print("\n")
-#    print("-------------------------------------")
-#    exit()
-#else:
-#    pass
-
 download_list = [f"CUUR0000{input_code}"] 
 
 name_series = data_dict["item_name"][f"{input_code}"]
@@ -129,7 +112,22 @@ else:
 
 dates = (init_date, end_date)  
 
-### DATA DOWNLOAD
+## Transform series (original, mom or yoy)
+
+transf_list = ["original", "Original", "ORIGINAL", "mom", "MoM", "MOM", "yoy", "YoY", "YOY"]
+input_transf = input("Please input transformation (Original, MoM, YoY): ")
+if input_transf not in transf_list:
+    print("-------------------------------------")
+    print("\n")
+    print("Unknown transformation. Please try again.")
+    print("\n")
+    print("-------------------------------------")
+    exit()
+else:
+    pass
+
+
+### DATA DOWNLOAD TO FILE
 
 headers = {'Content-type': 'application/json'}
 data = json.dumps({"seriesid": download_list,"startyear":dates[0], "endyear":dates[1],"catalog":False, "calculations":False, "annualaverage":False, "registrationkey":api_key})
@@ -141,7 +139,7 @@ p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data
 # Date index from first series
 date_list_unformatted = pd.to_datetime([f"{i['year']}-{i['period'][1:]}-01" for i in p[0]['data']])
 
-date_list = (datetime.datetime.strftime(i, "%m-%Y") for i in date_list_unformatted)
+date_list = (datetime.datetime.strftime(i, "%m/%d/%Y") for i in date_list_unformatted)
 
 # Empty dataframe to fill with values
 df = pd.DataFrame()
@@ -153,7 +151,7 @@ for s in p:
         data = [i['value'] for i in s['data']]
         ).astype(float).iloc[::-1]
 
-# Defining and saving file
+### SAVING FILE
 for series in p:
     seriesId = series['seriesID']
     file_name = seriesId + '.xlsx'
@@ -163,22 +161,37 @@ for series in p:
     df.values.tolist()
     df.to_excel(save_path)
 
-# Creating a chart
+### TRANSFORMING SERIES
+
+#monthly
+
+monthly_return = df.pct_change(periods=1)*100
+
+#yearly 
+
+yearly_return = df.pct_change(periods=12)*100
+
+### GENERATING CHART
 fig, ax = plt.subplots() # enables us to further customize the figure and/or the axesW
 
 ax.xaxis.set_major_locator(plt.MaxNLocator(4)) #editing number of ticks in the axis (https://stackoverflow.com/questions/6682784/reducing-number-of-plot-ticks/6682846#6682846)
    
 #plt.tight_layout() # ensures all areas of the chart are visible by default (fixes labels getting cut off)
 
+y_label = input_transf
+
 ax.set_xlabel("Dates")
-ax.set_ylabel("y label") #TODO:
+ax.set_ylabel(y_label) 
+
+# Choosing the correct series
 
 df.plot(title=name_series, ax=ax)
 
 plt.show()
 
+
+
+
+
 exit()
-
-
-
 
